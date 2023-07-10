fb7723103a95ed9a7a6468c3ff2a131f4d8fd16a
progaurav052 <139094275+progaurav052@users.noreply.github.com>
2023-07-10 10:23:44 +0530

    Add files via upload

---
+++ server/models/Post.js
@@ -0,0 +1,29 @@
+const mongoose=require("mongoose");
+
+const schema=mongoose.Schema;
+
+const PostSchema=new schema(
+    {
+        title:{
+            type:String,
+            required:true,
+        },
+        body:{
+            type:String,
+            required:true,
+        },
+        createdAt:{
+            type: Date,
+            default: Date.now
+
+        },
+        updatedAt:{
+            type: Date,
+            default: Date.now
+        }
+
+    }
+);
+
+module.exports=mongoose.model('Post',PostSchema);
+
